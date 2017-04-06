754f0f9572b5d67e5d99cf0edb2183349c051db4
tonyejack <jtonye@ymail.somc>
2017-04-05 21:53:18 -0400

    Modfied the post button

--- app.js
+++ app.js
@@ -2,47 +2,115 @@
 
 var express = require('express'),
     path = require('path'),
+    bodyParser = require('body-parser'),
+    EventEmitter = require('events'),
     port = 5000,
+    screenName = 'tonyejack1',
     app = express(),
     fs = require('fs'),
     auth = require('./public/js/config.js'),
     Twit = require('twit'),
     authConfig = auth.config,
-    config = {consumer_key: authConfig.consumer_key,
-                consumer_secret: authConfig.consumer_secret,
-                access_token: authConfig.access_token,
-                access_token_secret: authConfig.access_token_secret,
-                timeout_ms : authConfig.timeout_ms },
-
-    T = new Twit(config),
+    multer = require('multer'),
+    upload = multer(),
+    T = new Twit(authConfig),
     templates = path.join(__dirname, 'templates'),
     staticFiles = path.join(__dirname, '/public' );
 
 
-app.use('/static',express.static(staticFiles));
 // app.engine('html', require('ejs').renderFile);
 // app.set('view engine', 'html');
+
+
+app.use('/static',express.static(staticFiles));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.set('view engine', 'jade');
 app.set('views', templates);
 
+var streamTweets = T.stream('statuses/home_timeline');
 
-app.get('/', function(req, res){
+app.get('/home', function(req, res){
    res.sendFile('index.html', {root: __dirname});
 });
 
-app.get('/home', function(req, res){
-    var user = {handle: '@tj!', sign: {url:'/sign-out', text:'Sign Out'}};
-    res.render('index_', {user: user});
+
+app.get('/sign-in', function (req, res) {
+   res.render('sign_in')
 });
 
-app.get('/sign-out', function (req, res) {
-    var user = {handle: undefined, sign: {url:'/home', text:'Sign In'}};
-   res.render('sign_out', {user: user})
+app.get('/', function(req, res){
+    res.render('sign_in');
+});
+
+app.get('/user', upload.array(), function (req, res) {
+    var user, data;
+    user = {handle: screenName, sign: {url:'/sign-out', text:'Sign Out'}};
+
+    setImmediate(function () {
+        T.get('statuses/home_timeline', function (err, data, response) {
+            res.render('index_', {user: user, content: data});
+        });
+        // Get the followers from twitter
+        // T.get('followers/ids', { screen_name: screenName },  function (err, data, response) {
+        //     console.log(data);
+        //
+        // });
+    });
+});
+
+
+app.post('/', upload.array(), function (req, res, next) {
+    var body, name, password;
+    body = req.body;
+    name = body.user_name;
+    password = body.password;
+
+    if(name === undefined || password === undefined || password === '' || name === ''){
+        res.redirect('/sign-in');
+    }else{
+        res.redirect('/user');
+    }
+});
+
+
+app.get('/sign-in', function (req, res) {
+    var user = {handle: 'Tommy'};
+    res.render('sign_in', {user: user});
 });
 
 
 app.listen(port, function () {
    console.log("Server running on port %d", port);
+});
+
+
+app.post('/post-tweet', upload.array() , function (req, res, next) {
+    var body, tweet, success, msg;
+    body = req.body;
+    tweet = body['text-area'];
+    success = true;
+
+    if (tweet !== undefined && tweet !== ""){
+        setImmediate(function () {
+            T.post('statuses/update', {status: tweet}, function (err, data, response) {
+                if (err) {
+                    success = false;
+                    console.error(err);
+                }
+                console.log(data);
+                console.dir(response);
+            });
+            if(success === true) {
+                msg = 'Tweet Sent';
+            }else {
+                msg = 'Tweet Unable to be delivered';
+            }
+            res.render('index_', {success:success, msg: msg});
+        });
+    }else{
+        res.redirect('back');
+    }
 });
 
 //
--- public/js/twitter-app.js
+++ public/js/twitter-app.js
@@ -1,3 +1,51 @@
 /**
  * Created by tonye on 2017-03-28.
  */
+
+$(document).ready(function(){
+    var $textArea =  $("#tweet-textarea");
+    var $alertSuccess = $('.alert-success');
+    var $alertWarning = $('.alert-warning');
+    var alerts = [$alertSuccess, $alertWarning];
+    // var user,pass;
+    // $("#submit").click(function(){
+    //     user=$("#user").val();
+    //     pass=$("#password").val();
+    //     $.post("http://localhost:3000/login",{user: user,password: pass}, function(data){
+    //         if(data==='done')
+    //         {
+    //             alert("login success");
+    //         }
+    //     });
+    // });
+
+
+    alerts.forEach(function ($element) {
+        if ($element.length !== 0 && $element.css('display') !== 'None'){
+            alert($element.text());
+        }
+    });
+
+    $textArea.on('keydown', function () {
+        $textArea.css('border', 'None');
+    });
+
+    $('.post-form').submit(function (event) {
+
+        var tweet = $textArea.val();
+
+        if(tweet === undefined || tweet === "") {
+            $textArea.css('border', '2px solid red');
+            event.preventDefault();
+        }else{
+            $textArea.css('border', 'None');
+        }
+    });
+
+    if ("/sign-in" === window.location.pathname){
+        $('.sign-out').hide();
+    }
+
+});
+
+
